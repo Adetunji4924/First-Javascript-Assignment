764e16266f17d188c5cad62be82be1987fde1b5c
Adewole Muhammed <adewoleadetunji77@gmail.com>
2022-06-24 09:42:57 +0100

    My first Javascript assignment

---
+++ calc.js
@@ -0,0 +1,7 @@
+
+function myFunc(){
+    alert("Hello World");
+}
+document.getElementById("name").innerHTML= "Adewole Muhammed";
+document.getElementById("height").innerHTML= "5'8";
+document.getElementById("country").innerHTML= "Nigeria";
